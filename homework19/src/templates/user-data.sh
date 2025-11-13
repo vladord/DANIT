32251a8780a9868224d13e18188eea65b71ea577
Vlad Ordynskyi <vladord@ukr.net>
2025-11-13 17:19:44 +0200

    git fix after rehead

--- homework19/src/templates/user-data.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-PACKAGENAME="nginx"
-apt update && apt install $${PACKAGENAME} -y
-systemctl enable nginx && systemctl start nginx
-echo "Hello from ${environment}" > /var/www/html/index.nginx-debian.html
- 
